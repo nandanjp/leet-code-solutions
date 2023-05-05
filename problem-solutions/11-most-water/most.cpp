48a57a785e3fc9a3d89e382de8bce281c0afcb32
Nandan <nandan.jp17@gmail.com>
2023-05-05 17:32:30 -0600

    Finished 11 - Most Water Area question

--- problem-solutions/11-most-water/most.cpp
+++ problem-solutions/11-most-water/most.cpp
@@ -7,34 +7,24 @@ using namespace std;
 int maxArea(vector<int> &height)
 {
 	size_t size = height.size();
-	if (size == 2)
-		return height[0] > height[1] ? height[1] : height[0];
-
 	size_t left = 0, right = size - 1;
-	int area = -1, currArea, nextArea;
-
+	int area = -1, currArea;
 	while (left < right)
 	{
-		currArea = (right - left) * (height[left] > height[right] ? height[right] : height[left]);
-		if (abs(currArea) > area)
-			area = currArea;
+		currArea = std::min(height[left], height[right]) * (right - left);
 
-		nextArea = (right - left - 1) * (height[left + 1] > height[right] ? height[right] : height[left + 1]);
-
-		if (abs(nextArea) > area)
-			++left, right = size - 1;
+		area = std::max(area, currArea);
+		if (height[left] > height[right])
+			--right;
 		else
-			--right, left = 0;
-
-		std::cout << "left = " << left << ", right = " << right << ", currArea = " << currArea << ", nextArea = " << nextArea << ", area = " << area << std::endl;
+			++left;
 	}
-
 	return area;
 }
 
 int main()
 {
-	vector<int> heights{2, 3, 4, 5, 18, 17, 6};
+	vector<int> heights{1, 8, 6, 2, 5, 4, 8, 3, 7};
 	int maxA = maxArea(heights);
 
 	std::cout << "Max Area of Water Possible is: " << maxA << std::endl;
